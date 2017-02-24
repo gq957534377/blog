dda2c59bc29263ccdf2a01471195d5fc4bf4b842
guoqing <guoqing@qq.com>
2017-02-24 17:37:14 +0800

    初始化

--- resources/views/default/footer.blade.php
+++ resources/views/default/footer.blade.php
@@ -3,9 +3,9 @@
         <div class="row">
             <div class="col-md-12 text-center">
                 <p style="margin-top:15px;">
-                    <strong>Powered by <a href="http://moell.cn" target="_blank">Moell Blog</a></strong>
+                    <strong>Powered by <a href="http://blog.guoq.xin" target="_blank">GQ1994 庆欧巴~</a></strong>
                     &nbsp;&nbsp;
-                    <a href='https://github.com/moell-peng/moell-blog' target="_blank">
+                    <a href='https://github.com/gq957534377' target="_blank">
                         <span class="icon-github" style="padding-right:20px;"></span>
                     </a>
                 </p>
